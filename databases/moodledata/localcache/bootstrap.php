771d0f3d35395e6c1789f33d696145bcb32b9c2f
Daniel Gonzalo <118071772+DanielLlumigusin@users.noreply.github.com>
2025-06-30 23:02:55 -0500

    Cache de Configuracion

---
+++ databases/moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'XmNH0hogQr0tkuwsrBtW2dSU5FJCqCaTlocalhost';
+$CFG->bootstraphash = '3b7c395037bee74c998a16f1934edc2a';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
